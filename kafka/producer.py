698ea949812bb7537aeeec2416644c80a2009429
NAVIN TIGGA <146591653+NaVTOKEN@users.noreply.github.com>
2026-03-28 04:50:21 +0530

    Update producer.py

--- kafka/producer.py
+++ kafka/producer.py
@@ -1,8 +1,8 @@
 
-from kafka import KafkaProducer
 import json
 import time
 import random
+from kafka import KafkaProducer
 
 producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
@@ -11,14 +11,22 @@ producer = KafkaProducer(
 
 cities = ["Delhi", "Mumbai", "Bangalore"]
 
-while True:
-    trip = {
-        "trip_id": random.randint(1000, 9999),
-        "city": random.choice(cities),
-        "fare": random.randint(100, 1000),
-        "status": random.choice(["completed", "cancelled"])
-    }
-    
-    producer.send("uber_trips", trip)
-    print("Sent:", trip)
-    time.sleep(2)
+USE_SAMPLE_FILE = True   
+
+if USE_SAMPLE_FILE:
+    with open("data/sample_trips.json") as f:
+        for line in f:
+            producer.send("uber_trips", json.loads(line))
+            print("Sent:", line.strip())
+            time.sleep(1)
+else:
+    while True:
+        trip = {
+            "trip_id": random.randint(1000, 9999),
+            "city": random.choice(cities),
+            "fare": random.randint(100, 1000),
+            "status": random.choice(["completed", "cancelled"])
+        }
+        producer.send("uber_trips", trip)
+        print("Sent:", trip)
+        time.sleep(2)
